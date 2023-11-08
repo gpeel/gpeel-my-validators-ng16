7ac83ec2d427ce2b3f43e448ab903b53dc40479a
Gauthier PEEL <gauthier.peel@gmail.com>
2023-11-08 18:22:51 +0100

    comment and activating errors on blur only (not dirty)

--- projects/reactive3-flexible-validators/src/app/app.module.ts
+++ projects/reactive3-flexible-validators/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AbstractControl, FormArray, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
-import {MY_ALERT_SERVICE_API, MY_MESSAGES_SERVICE_API, MyValidatorsModule} from '@gpeel/my-validators';
+import {
+  MY_ALERT_SERVICE_API,
+  MY_MESSAGES_SERVICE_API,
+  MY_SHOW_ERROR_MSG_FUNCTION_API,
+  MyValidatorsModule
+} from '@gpeel/my-validators';
 import {PlogModule} from '@gpeel/plog';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {environment} from '../environments/environment';
@@ -51,13 +56,19 @@ import {TypicalValidatorsService} from './validators/typical-validators.service'
     // You can change globally this function here,
     // or you can make a local change for one specific input with the use showErrorFunction @Input
     // on <my-error-msg [showErrorFunction]="fn" > or on <input myErrorMsg  [showErrorFunction]="fn" >
-    // {
-    //   provide: MY_SHOW_ERROR_MSG_FUNCTION_API,
-    //   // default value:
-    //   // useValue: (control: AbstractControl) => (control.dirty || control.touched)
-    //   // not showing error until blurred
-    //   useValue: (control: AbstractControl) => control.touched
-    // }
+    {
+      provide: MY_SHOW_ERROR_MSG_FUNCTION_API,
+      // default value:
+      // useValue: (control: AbstractControl) => (control.dirty || control.touched)
+      // not showing error until blurred
+      useValue: (control: AbstractControl) => control.touched
+      /**
+       * If you want a Strategy when you reveal the erors when the form is submitted or field blurred.
+       * PB : With Angular there is no way to know if the form is submitted or not.
+       * So you must do part of the job behind the button handler which submit the form.
+       * The rest of the job is done by this function with useValue: (control: AbstractControl) => control.touched
+       */
+    }
   ],
   bootstrap: [AppComponent]
 })
